ffaf395e418e8d2a8f2fad319fab586d5e20e3ae
JakeMaz <jakemazanowski@gmail.com>
2022-05-19 14:02:38 -0400

    commit

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const {Sequelize} = require("sequelize");
 
-const db = new Sequelize('postgres://qigqoeiueiexgf:b492fa8ad8f82d4da5b3a44b6a32a26033f71cfef178e92e7be083d318833838@ec2-54-210-128-153.compute-1.amazonaws.com:5432/d1op435139ll2p', {
+const db = new Sequelize('postgres://ypgnjxfnyrvgqo:372c54040709cbb530878762af2ebd9093b64d778b86b4a2131977b624b23904@ec2-3-228-235-79.compute-1.amazonaws.com:5432/d6m0d9pucr29fc', {
     dialect: 'postgres'
 });
 
