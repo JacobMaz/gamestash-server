972c0967daa19f7d16abd8650108fa45b594c76b
JakeMaz <jakemazanowski@gmail.com>
2022-05-19 14:00:44 -0400

    tsting dbstring

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const {Sequelize} = require("sequelize");
 
-const db = new Sequelize(process.env.DATABASE_URL, {
+const db = new Sequelize('postgres://qigqoeiueiexgf:b492fa8ad8f82d4da5b3a44b6a32a26033f71cfef178e92e7be083d318833838@ec2-54-210-128-153.compute-1.amazonaws.com:5432/d1op435139ll2p', {
     dialect: 'postgres'
 });
 
