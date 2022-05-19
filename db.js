0d38e655be8379bb80b502c6ff8012336218b497
JakeMaz <jakemazanowski@gmail.com>
2022-05-19 14:04:16 -0400

    pain

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const {Sequelize} = require("sequelize");
 
-const db = new Sequelize('postgres://ypgnjxfnyrvgqo:372c54040709cbb530878762af2ebd9093b64d778b86b4a2131977b624b23904@ec2-3-228-235-79.compute-1.amazonaws.com:5432/d6m0d9pucr29fc', {
+const db = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres'
 });
 
